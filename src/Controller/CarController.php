c4b390a045fa1c7f22f3f903ec29ef007fea0d98
Jonathan Leprini <leprini.jonathan@protonmail.com>
2024-04-16 11:38:41 +0200

    Ajout d'une fonction de suppression d'une voiture au sein de CarController

--- src/Controller/CarController.php
+++ src/Controller/CarController.php
@@ -3,13 +3,16 @@
 namespace App\Controller;
 
 use App\Repository\CarRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class CarController extends AbstractController
 {
+    /**
+     * Page d'accueil
+     */
     #[Route('/voiture/{id}', name: 'car.show')]
     public function index(int $id, CarRepository $carRepository): Response
     {
@@ -22,5 +25,23 @@ class CarController extends AbstractController
         return $this->render('car/index.html.twig', [
             'car' => $car,
         ]);
+    }
+
+    /**
+     * Supprimer un véhicule
+     */
+    #[Route('/voiture/{id}/supprimer', name: 'car.delete')]
+    public function delete(int $id, CarRepository $carRepository, EntityManagerInterface $entityManager): Response
+    {
+        $car = $carRepository->find($id);
+
+        if (!$car) {
+            return $this->redirectToRoute('home.show');
+        }
+
+        $entityManager->remove($car);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('home.show');
     }
 }
